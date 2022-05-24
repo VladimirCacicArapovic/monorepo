fe6f7eac77e9267f6de5b45a3d077de0f0c54035
cacic-arapovicv <vlad.cacicarapovic@vodafone.com>
2022-05-24 15:19:30 +0100

    Testing

--- apps/first-app/src/app/app.tsx
+++ apps/first-app/src/app/app.tsx
@@ -5,7 +5,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <>
-      <NxWelcome title="first-app" />
+      <NxWelcome title="first-app" /> ONE
       <div />
     </>
   );
